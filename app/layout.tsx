c27378dd6e36e510b38298cce550f620ec6d8625
Vinayak Chaturvedi <racidmartyr228@gmail.com>
2025-11-03 23:10:40 +0530

    Added the exploreBtn and the Navbar in the Homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Schibsted_Grotesk, Martian_Mono } from "next/font/google";
 import "./globals.css";
 import LightRays from "@/components/LightRays";
+import Navbar from "@/components/Navbar";
 
 const schibstedGrotesk = Schibsted_Grotesk({
   variable: "--font-schibested-grotesk",
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${schibstedGrotesk.variable} ${martianMono.variable} min-h-screen antialiased`}
       >
+      <Navbar />
           <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
               <LightRays
                   raysOrigin="top-center-offset"
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
+import ExploreBtn from "@/components/ExploreBtn";
+
 const Page = () => {
     return (
         <section>
-            <h1>
-                Welcome to NextJS 16
+            <h1 className="text-center">
+                The Hub for Every Dev <br /> Event You Can't Miss
             </h1>
+            <p className="text-center mt-5">
+                Hackathons, Meetups, and Conferences, All in one Place
+            </p>
+            <ExploreBtn />
+
+            <div className="events">
+                {[1, 2, 3, 4, 5].map((event) => (
+                    <li key={event}>Event {event}</li>
+                ))}
+            </div>
         </section>
     );
 };
