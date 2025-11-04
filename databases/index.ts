da46cdf023dbb91bfb52bd3eefdcddb89ca05579
Vinayak Chaturvedi <racidmartyr228@gmail.com>
2025-11-04 17:05:54 +0530

    Implement Database Models

---
+++ databases/index.ts
@@ -0,0 +1,5 @@
+export { default as Events } from './events';
+export { default as Booking } from './bookings';
+
+export type { IEvent } from './events';
+export type { IBooking } from './bookings';
