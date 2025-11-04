da72795393862d8df312b5ee10fec97cbf0a87d4
Vinayak Chaturvedi <racidmartyr228@gmail.com>
2025-11-04 16:33:06 +0530

    implemented-posthog

---
+++ instrumentation-client.js
@@ -0,0 +1,10 @@
+// instrumentation-client.js
+import posthog from 'posthog-js'
+
+posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    defaults: '2025-05-24'
+});
+
+
+posthog.capture('my event', { property: 'value' })
